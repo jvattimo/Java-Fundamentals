5a3948342fa93dc38abc9deb99b9368e10a48cc9
Joe Vattimo <jvattimo@westmonroepartners.com>
2023-06-14 14:46:08 +0100

    Complete Exercise 4

--- src/labs_examples/fundamentals/labs/Exercise_04.java
+++ src/labs_examples/fundamentals/labs/Exercise_04.java
@@ -16,6 +16,29 @@ public class Exercise_04 {
         System.out.println("int i is: " + i);
 
         // write your code below
+        boolean isEmpty = false;
+        System.out.println(isEmpty);
+
+        char letter = 'a';
+        System.out.println(letter);
+
+        byte byteVal = 45;
+        System.out.println(byteVal);
+
+        short shortVal = 3048;
+        System.out.println(shortVal);
+
+        int intVal = 827934;
+        System.out.println(intVal);
+
+        long longVal = 8932749839473l;
+        System.out.println(longVal);
+
+        float floatVal = 89327.083084938439f;
+        System.out.println(floatVal);
+
+        double doubleVal = 123.456;
+        System.out.println(doubleVal);
 
     }
 
