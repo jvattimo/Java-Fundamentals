5acacc4cbfcd04049175a94363885f4618f73c15
Joe Vattimo <jvattimo@westmonroepartners.com>
2023-07-11 12:30:48 +0100

    Exercise 5 - datatypes operators, Complete

--- src/labs_examples/datatypes_operators/labs/Exercise_05.java
+++ src/labs_examples/datatypes_operators/labs/Exercise_05.java
@@ -17,8 +17,39 @@ class LogicalOperators {
             System.out.println("a or b is true");
         }
 
-        // write your code below
+        // example of "AND"
+        boolean c = true;
+        boolean d = true;
+        if (c & d) {
+            System.out.println("c and d is true");
+        }
 
+        // example of "short circut AND"
+        boolean e = true;
+        boolean f = true;
+        if (e && f) {
+            System.out.println("e is true and f is true");
+        }
+
+        // example of "short circut OR"
+        boolean g = true;
+        boolean h = true;
+        if (g || h) {
+            System.out.println("g is true and h is true");
+        }
+        // example of "XOR"
+        boolean i = false;
+        boolean j = true;
+        if (i ^ j) {
+            System.out.println("i is false and j is true");
+        }
+
+        // example of "NOT"
+        boolean k = true;
+        boolean l = false;
+        if (!l) {
+            System.out.println("l is false");
+        }
     }
 
 }
