8a3c1a0f1e53bdd441a790776c7a885ebf443eb1
Joe Vattimo <jvattimo@westmonroepartners.com>
2023-07-11 12:40:26 +0100

    Exercise 6 - datatypes_operators, Complete

--- src/labs_examples/datatypes_operators/labs/Exercise_06.java
+++ src/labs_examples/datatypes_operators/labs/Exercise_06.java
@@ -13,6 +13,13 @@ public class Exercise_06 {
     public static void main(String[] args) {
 
         // write code here
-
+        double Pi = 3.141592654;
+        double r = 3.14;
+        double r2 = r*r;
+        int h = 5;
+        double SurfaceArea = 2*Pi*r*h + 2*Pi*r2;
+        double Volume = Pi*r2*h;
+        System.out.println(SurfaceArea);
+        System.out.println(Volume);
+        }
     }
-}
