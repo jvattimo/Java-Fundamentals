8438c9628b7866bbf322a9a310db27d2e0869bb0
Joe Vattimo <jvattimo@westmonroepartners.com>
2023-07-11 11:36:56 +0100

    Exercise 3 - Datatypes_Operators, Complete

--- src/labs_examples/datatypes_operators/labs/Exercise_03.java
+++ src/labs_examples/datatypes_operators/labs/Exercise_03.java
@@ -11,6 +11,17 @@ class ArithmeticOperators {
     public static void main(String[] args) {
 
         // write your code below
+        int x = 15;
+        int y = x + 3;
+        int z = x - 3;
+        int l = x * 58;
+        int w = x / 3;
+        int c = x % 4;
+        System.out.println(y);
+        System.out.println(z);
+        System.out.println(l);
+        System.out.println(w);
+        System.out.println(c);
 
     }
 
