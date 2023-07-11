5477434527c161c465694c42db8cc02d7df0ebd2
Joe Vattimo <jvattimo@westmonroepartners.com>
2023-07-11 12:15:53 +0100

    Exercise 4 - Datatype Operators, Complete

--- src/labs_examples/datatypes_operators/labs/Exercise_04.java
+++ src/labs_examples/datatypes_operators/labs/Exercise_04.java
@@ -17,7 +17,33 @@ class RelationalOperators {
             System.out.println("a is less than b");
         }
 
-        // write your code below
+        // example of "greater than"
+        int c = 4;
+        int d = 3;
+        if (c > d){
+            System.out.println("c is greater than d");
+        }
+
+        // example of "less than or equal to"
+        int e = 7;
+        int f = 9;
+        if (e <= f) {
+            System.out.println("e is less than or equal to f");
+        }
+
+        // example of "greater than or equal to"
+        int g = 15;
+        int h = 9;
+        if (g >= h) {
+            System.out.println("g is greater than or equal to h");
+        }
+
+        // example of "equal to"
+        int i = 15;
+        int j = 15;
+        if (i == j) {
+            System.out.println("i is equal to j, fuck you you don't own i and j, bitch");
+        }
 
     }
 
