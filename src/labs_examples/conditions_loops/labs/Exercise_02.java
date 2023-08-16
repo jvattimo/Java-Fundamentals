60881b42316951a8041598def2bf0426a375fc98
Joe Vattimo <jvattimo@westmonroepartners.com>
2023-08-16 10:54:55 +0100

    Exercise 2 Complete

--- src/labs_examples/conditions_loops/labs/Exercise_02.java
+++ src/labs_examples/conditions_loops/labs/Exercise_02.java
@@ -1,5 +1,7 @@
 package labs_examples.conditions_loops.labs;
 
+import java.util.Scanner;
+
 /**
  * Conditions and Loops Exercise 2: Days of the week
  *
@@ -14,8 +16,40 @@ package labs_examples.conditions_loops.labs;
 public class Exercise_02 {
 
     public static void main(String[] args) {
-    
-        // write completed here
+
+        // create scanner
+        Scanner scanner = new Scanner(System.in);
+        // prompt user
+        System.out.print("Enter a number between 1 and 10 to receive a response: ");
+        // assign input to variable as int
+        int number = scanner.nextInt();
+        // Check number and return day of the week
+        String day;
+        if (number == 1) {
+            day = "Monday";
+        } else if (number == 2) {
+            day = "Tuesday";
+        } else if (number == 2) {
+            day = "Tuesday";
+        }else if (number == 3) {
+            day = "Wednesday";
+        }else if (number == 4) {
+            day = "Thursday";
+        }else if (number == 5) {
+            day = "Friday";
+        }else if (number == 6) {
+            day = "Saturday";
+        }else if (number == 7) {
+            day = "Sunday";
+        }else {
+            day = "Other";
+        }
+
+        // Print out result
+        System.out.println("The number corresponds to the day " + day);
+
+        // Close scanner
+        scanner.close();
 
     }
 }
