d1dda7b40895ddea37eaf9f4128b0cdf55c66949
Joe Vattimo <jvattimo@westmonroepartners.com>
2023-08-16 11:39:29 +0100

    Exercise 4 complete

--- src/labs_examples/conditions_loops/labs/Exercise_04.java
+++ src/labs_examples/conditions_loops/labs/Exercise_04.java
@@ -8,5 +8,11 @@ package labs_examples.conditions_loops.labs;
  */
 
 public class Exercise_04 {
+    public static void main(String[] args) {
+        System.out.println("Even numbers from 1 to 100:");
+        for (int i = 2; i <= 100; i += 2) {
+            System.out.println(i);
+        }
+    }
 
 }
