a16bab462a613fa0b2f17adbaa885367be168456
jvattimo <joey_vattimo@yahoo.com>
2023-08-21 09:48:45 +0100

    Exercise 3 Complete

--- src/labs_examples/arrays/labs/Exercise_03.java
+++ src/labs_examples/arrays/labs/Exercise_03.java
@@ -3,7 +3,7 @@ package labs_examples.arrays.labs;
 /**
  *  2D Array
  *
- *      Creat and populate a 5x5 2D array with multiples of 3, starting with 3. Once populated, print out the results.
+ *      Create and populate a 5x5 2D array with multiples of 3, starting with 3. Once populated, print out the results.
  *
  *      The output should look something like this:
  *
@@ -18,6 +18,23 @@ package labs_examples.arrays.labs;
 public class Exercise_03 {
 
     public static void main(String[] args) {
-        
+
+        int[][] array = new int[5][5];
+        int currentNumber = 3;
+
+        //populate with multiples of 3
+        for (int row = 0; row < 5; row++) {
+            for (int col = 0; col < 5; col++) {
+                array[row][col] = currentNumber;
+                currentNumber += 3;
+            }
+        }
+        //print array
+        for (int row = 0; row < 5; row++) {
+            for (int col = 0; col < 5; col++) {
+                System.out.print(array[row][col] + " ");
+            }
+            System.out.println();
+        }
+       }
     }
-}
